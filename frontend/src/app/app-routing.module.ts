05afcca18b8c141a4d70dcb500e6d996a0053fc6
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-10-29 15:38:30 +0100

    Dodano podstawowy routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
+import { AccountComponent } from './components/account/account.component'
+import { LocalComponent } from './components/local/local.component'
+import { TeamComponent } from './components/team/team.component'
+import { WorkspaceComponent } from './components/workspace/workspace.component'
 
-const routes: Routes = []
+const routes: Routes = [
+  { path: 'team',      component: TeamComponent },
+  { path: 'workspace', component: WorkspaceComponent },
+  { path: 'account',   component: AccountComponent },
+  { path: 'local',     component: LocalComponent },
+  // Will add main page later
+  { path: '',          redirectTo: 'workspace', pathMatch: 'full' },
+  // Will add error page later
+  { path: '**',        redirectTo: 'workspace' },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
