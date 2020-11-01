f351e72ea6e23ce1028053530504189cbe8a6ee6
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-11-01 12:03:13 +0100

    Dodano klasę User + poszerzono Team

--- frontend/src/app/classes/team.ts
+++ frontend/src/app/classes/team.ts
@@ -1,3 +1,8 @@
 export class Team {
-  public constructor(public name: string) { }
+  public constructor(
+    public readonly name: string,
+    public readonly image: ImageBitmap,
+    public readonly description: string,
+    public readonly leader: any, /* User */
+    public readonly members: any[] /* User[] */ ) { }
 }
---
+++ frontend/src/app/classes/user.ts
@@ -0,0 +1,7 @@
+class User {
+  constructor(
+    public readonly firstname: string,
+    public readonly surname: string,
+    public readonly email: string,
+    public readonly avatar: ImageBitmap) { }
+}
