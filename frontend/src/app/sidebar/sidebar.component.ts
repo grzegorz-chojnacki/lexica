b01af4bbf503560546a507cc8d70ecbcfda9e7b0
nwojtania <natalia-wojtania@wp.pl>
2020-11-01 21:52:39 +0100

    sidebar

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http'
 import { Component, OnInit, Input } from '@angular/core'
+import { from } from 'rxjs'
 import { AccountComponent } from '../components/account/account.component'
+import { TeamService } from 'src/app/services/team.service'
+import { Team } from 'src/app/classes/team'
 
 @Component({
   selector: 'app-sidebar',
@@ -11,8 +14,12 @@ import { AccountComponent } from '../components/account/account.component'
 export class SidebarComponent implements OnInit {
   // @Input('av') av: any;
   public userAccount: AccountComponent = new AccountComponent()
-  public constructor() { }
-  public ngOnInit(): void { }
+  public teams: Team[] = []
+
+  public constructor(private readonly teamService: TeamService) { }
+  public ngOnInit(): void {
+  this.teamService.teams.subscribe(teams => this.teams = teams)
+  }
 
   // console.log(this.av);
 
