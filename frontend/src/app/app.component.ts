0533d623acc2160d601af9ba53e93f2386ab9675
nwojtania <natalia-wojtania@wp.pl>
2020-11-12 21:26:57 +0100

    nazwa uzytkownika w mat-toolbar korzysta z serwisu

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core'
+import { User } from 'src/app/classes/user'
+import { UserService } from 'src/app/services/user.service'
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,10 @@ import { Component } from '@angular/core'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public user!: User
+
+  public constructor(private userService: UserService) {
+
+    this.userService.loggedUser.subscribe(u => this.user = u)
+  }
 }
