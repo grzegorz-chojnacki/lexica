2b9d93428161bb53a6fb2c3d4eb4822ebe44e057
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-11-15 18:43:14 +0100

    Małe zmiany

--- frontend/src/app/services/previous-route.service.ts
+++ frontend/src/app/services/previous-route.service.ts
@@ -1,25 +1,25 @@
-import { Injectable } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Injectable } from '@angular/core'
+import { Router, NavigationEnd } from '@angular/router'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreviousRouteService {
 
-  private previousUrl!: string;
-  private currentUrl: string;
+  private previousUrl!: string
+  private currentUrl: string
 
   public constructor(private router: Router) {
-    this.currentUrl = this.router.url;
+    this.currentUrl = this.router.url
     router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-        this.previousUrl = this.currentUrl;
-        this.currentUrl = event.url;
-      };
-    });
+        this.previousUrl = this.currentUrl
+        this.currentUrl = event.url
+      }
+    })
   }
 
   public getPreviousUrl(): string {
-    return this.previousUrl;
+    return this.previousUrl
   }
 }
