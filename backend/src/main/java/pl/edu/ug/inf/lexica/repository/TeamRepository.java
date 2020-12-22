3c37cd188dfb06ac4ee990e3e47d1c2b42e413e3
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-12-22 22:01:17 +0100

    Removed unused query

--- backend/src/main/java/pl/edu/ug/inf/lexica/repository/TeamRepository.java
+++ backend/src/main/java/pl/edu/ug/inf/lexica/repository/TeamRepository.java
@@ -1,12 +1,9 @@
 package pl.edu.ug.inf.lexica.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.edu.ug.inf.lexica.domain.Team;
 
-import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,9 +11,4 @@ import java.util.Optional;
 public interface TeamRepository extends JpaRepository<Team, Integer> {
     Optional<Team> findById(Integer id);
     List<Team> findAll();
-
-    @Modifying
-    @Transactional
-    @Query(value = "UPDATE team SET name=?2, description=?3 WHERE id=?1", nativeQuery = true)
-    void save(long id, String name, String description);
 }
