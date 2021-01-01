497901d761004898394874ec928a29bc173015d0
nwojtania <natalia-wojtania@wp.pl>
2021-01-01 20:25:34 +0100

    not creating void simpleCard

--- frontend/src/app/components/task/simple-cards-adding/simple-cards-adding.component.ts
+++ frontend/src/app/components/task/simple-cards-adding/simple-cards-adding.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core'
 import { MatDialog } from '@angular/material/dialog'
 import { SimpleCardAddingComponent } from 'src/app/components/task/simple-card-adding/simple-card-adding.component'
 import { SimpleCard } from 'src/app/classes/task'
+import { FormBuilder } from '@angular/forms'
+import { TaskService } from 'src/app/services/task.service'
+
 @Component({
   selector: 'app-simple-card',
   templateUrl: './simple-cards-adding.component.html',
@@ -12,13 +15,24 @@ export class SimpleCardsAddingComponent implements OnInit {
   public obce!: string
   public narodowe!: string
   public simpleCards: SimpleCard[] = []
-  public constructor(private readonly dialog: MatDialog) { }
+  public checkoutForm
+
+  public constructor(
+  private readonly dialog: MatDialog,
+  private taskService: TaskService,
+  private formBuilder: FormBuilder) {
+    this.checkoutForm = this.formBuilder.group({
+      title: '',
+      description: ''
+    })
+   }
 
 
 
   public ngOnInit(): void { }
 
   public submit(): void {
+    console.log(this.checkoutForm)
   }
   public delete(): void {
   }
@@ -27,7 +41,10 @@ export class SimpleCardsAddingComponent implements OnInit {
     height: '500px', data: { obce: this.obce, narodowe: this.narodowe}, hasBackdrop: false})
 
     dialogRef.afterClosed().subscribe(result => {
+      if (result.obce.length === 0 || result.narodowe.length === 0) { }
+      else {
       this.simpleCards.push(new SimpleCard(result.obce, result.narodowe))
+      }
     })
    }
 }
--- frontend/src/app/services/task.service.ts
+++ frontend/src/app/services/task.service.ts
@@ -2,15 +2,18 @@ import { Injectable } from '@angular/core'
 import { BehaviorSubject, Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 
-import { SimpleCard, Task } from 'src/app/classes/task'
+import { Example, SimpleCard, Task } from 'src/app/classes/task'
 import { testTasks } from 'src/app/testData'
 import { HttpClient } from '@angular/common/http'
 import { lexicaURL } from '../lexica.properties'
+import { TaskType } from '../classes/task-type'
 
 export interface TaskForm {
   readonly name: string
   readonly description?: string
+  readonly examples: SimpleCard[]
   readonly image?: string
+  readonly taskType: TaskType
 }
 
 @Injectable({
@@ -28,11 +31,10 @@ export class TaskService {
   }
 
   // ToDo: Send this data to server and fetch new task list
-  // public createTask(form: TaskForm): void {
-  //   const newTask = new Task(form.name, [])
-
-  //   this.prependTaskSource(newTask)
-  // }
+   public createTask(form: TaskForm): void {
+     const newTask = new Task('a', form.name, form.examples, form.taskType, true, form.description)
+     this.prependTaskSource(newTask)
+   }
 
   private prependTaskSource(task: Task<SimpleCard>): void {
     this.taskSource.next([task, ...this.taskSource.getValue()])
