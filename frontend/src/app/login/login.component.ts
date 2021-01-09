bb2ea3426370bbaf9ee95ef845b8e5c84a7e6896
nwojtania <natalia-wojtania@wp.pl>
2021-01-09 20:00:48 +0100

    logowanie -> zalogujesz się jeśli hasło=lexica

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -9,6 +9,18 @@ import { first } from 'rxjs/operators'
 styleUrls: ['./login.component.scss'] })
 
 export class LoginComponent implements OnInit {
-    public constructor() { }
-    public ngOnInit(): void { }
+    public email!: string
+    public password!: string
+    public constructor(private router: Router) { }
+    public ngOnInit(): void {
+        this.email = ''
+        this.password = ''
+    }
+
+    public loginUser(): void {
+        console.log(this.password, this.email)
+        if (this.password === 'lexica') {
+            this.router.navigateByUrl('/workspace')
+        }
+    }
 }
