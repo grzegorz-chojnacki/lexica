d6c5ab9cdf7f2e41268a476566be5ae94196a85b
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-01-09 14:49:37 +0100

    Moved leader progress saving toggle to team settings

--- frontend/src/app/components/team/team-summary/team-summary.component.ts
+++ frontend/src/app/components/team/team-summary/team-summary.component.ts
@@ -1,9 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core'
 import { MatDialog } from '@angular/material/dialog'
-import { MatSlideToggleChange } from '@angular/material/slide-toggle'
 import { Team } from 'src/app/classes/team'
 import { TaskAddingComponent } from 'src/app/components/task/task-adding/task-adding.component'
-import { TeamService } from 'src/app/services/team.service'
 
 @Component({
   selector: 'app-team-summary',
@@ -14,25 +12,11 @@ export class TeamSummaryComponent implements OnInit {
   @Input() public team!: Team
 
   public constructor(
-    private readonly teamService: TeamService,
     private readonly dialog: MatDialog) { }
 
   public ngOnInit(): void { }
 
-  public toggleLeaderProgressActivation(event: MatSlideToggleChange): void {
-    event.checked
-      ? this.teamService.joinTeam(this.team.id)
-      : this.teamService.leaveTeam(this.team)
-  }
-
-  public isLeaderProgressActivated(): boolean {
-    return this.team.members.find(user => user.id === this.team.leader.id) !== undefined
-  }
-
   public newTaskDialog(): void {
     this.dialog.open(TaskAddingComponent, { width: '500px' })
   }
-
-  public acceptMembersDialog(): void { }
-
 }
