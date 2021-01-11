68a892bcefd376f07c6ee1b6468d09dabadfeb7c
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-01-11 20:53:00 +0100

    Prepared methods and dialog dependency

--- frontend/src/app/components/account/account.component.ts
+++ frontend/src/app/components/account/account.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { Location } from '@angular/common'
 import { User } from 'src/app/classes/user'
 import { UserService } from 'src/app/services/user.service'
+import { MatDialog } from '@angular/material/dialog'
 
 @Component({
   selector: 'app-account',
@@ -12,12 +13,18 @@ export class AccountComponent implements OnInit {
   public user!: User
 
   public constructor(
+    public readonly location: Location,
     private readonly userService: UserService,
-    public readonly location: Location) { }
+    private readonly dialog: MatDialog) { }
 
   public ngOnInit(): void {
     this.userService.user.subscribe(u => this.user = u)
   }
+
+  public changeFirstname(): void { }
+  public changeSurname(): void { }
+  public changeEmail(): void { }
+  public changePassword(): void { }
 
   public deleteAccount(): void { }
 }
