23b8ec185ba9982c20cc3994763daee28c2e5c04
nwojtania <natalia-wojtania@wp.pl>
2021-01-16 16:51:02 +0100

    team value in form, but how to get id?

--- frontend/src/app/components/task/simple-cards-adding/simple-cards-adding.component.ts
+++ frontend/src/app/components/task/simple-cards-adding/simple-cards-adding.component.ts
@@ -6,6 +6,7 @@ import { FormBuilder } from '@angular/forms'
 import { TaskService } from 'src/app/services/task.service'
 import { TeamService } from 'src/app/services/team.service'
 import { Team } from 'src/app/classes/team'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-simple-card',
@@ -19,7 +20,7 @@ export class SimpleCardsAddingComponent implements OnInit {
   public narodowe!: string
   public simpleCards: SimpleCard[] = []
   public checkoutForm =  this.formBuilder.group({
-   // team: this.team,
+    team: this.team,
     name: '',
     description: '',
     examples: this.simpleCards,
@@ -30,18 +31,20 @@ export class SimpleCardsAddingComponent implements OnInit {
   private readonly dialog: MatDialog,
   private taskService: TaskService,
   private teamService: TeamService,
-  private formBuilder: FormBuilder) { }
+  private formBuilder: FormBuilder,
+  public router: Router) { }
 
 
 
   public ngOnInit(): void {
-    this.teamService.getTeam('4a9d5686-293f-4838-b392-910908a76fb2')
+    this.teamService.getTeam('')
     .subscribe(team => this.team = team)
    }
 
   public submit(): void {
-  //  this.checkoutForm.updateOn.indexOf('examples')
-    this.checkoutForm.setValue({ name: this.checkoutForm.get('name')?.value,
+    this.checkoutForm.setValue({
+    team: this.team,
+    name: this.checkoutForm.get('name')?.value,
     description: this.checkoutForm.get('description')?.value,
     examples: this.simpleCards,
     taskType: SimpleCard
