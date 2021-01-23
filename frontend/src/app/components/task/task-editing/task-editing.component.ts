137cd5a3a909f8b59d8bfcac85317e7b5b04976b
nwojtania <natalia-wojtania@wp.pl>
2021-01-23 18:09:36 +0100

    task editing

---
+++ frontend/src/app/components/task/task-editing/task-editing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-editing',
+  templateUrl: './task-editing.component.html',
+  styleUrls: ['./task-editing.component.scss']
+})
+export class TaskEditingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
