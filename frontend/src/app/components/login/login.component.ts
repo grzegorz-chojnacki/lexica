0f42420a7ebabc38e14deb7b7d2470ea6a3a9194
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-01-27 21:27:30 +0100

    Refactored login page

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
+import { UserService } from 'src/app/services/user.service'
 
 @Component({
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-    public email = ''
-    public password = ''
-    public invalid = false
+  public loginForm = this.formBuilder.group({
+    email: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+  })
 
-    public constructor(private readonly router: Router) { }
+  public constructor(
+    private readonly userService: UserService,
+    private readonly formBuilder: FormBuilder,
+    private readonly router: Router) { }
 
-    public ngOnInit(): void { }
+  public ngOnInit(): void { }
 
-    public loginUser(): void {
-        console.log(this.password, this.email)
-        if (this.password === 'lexica' && this.email === 'jkowalski@example.com') {
-            this.router.navigateByUrl('/workspace')
-        } else { this.invalid = true }
-    }
+  public loginUser(): void {
+    const { email, password } = this.loginForm.value
+    this.userService.login(email, password).subscribe(
+      _ => this.router.navigate(['/workspace']),
+      _ => console.log('asdasd'))
+  }
 }
