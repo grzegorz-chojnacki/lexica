5ca902ba88e91d56b8896cff6c67931dd73c0cf3
nwojtania <natalia-wojtania@wp.pl>
2021-01-27 21:06:28 +0100

    fixed sorting members, logged on the top

--- frontend/src/app/pipes/sort-members.pipe.ts
+++ frontend/src/app/pipes/sort-members.pipe.ts
@@ -1,28 +1,19 @@
 import { Pipe, PipeTransform } from '@angular/core'
 import { User } from 'src/app/classes/user'
-import { TeamService } from 'src/app/services/team.service'
-import { UserService } from 'src/app/services/user.service'
-
 
 @Pipe({
   name: 'sortMembers'
 })
 export class SortMembersPipe implements PipeTransform {
-  public user!: User
-  public members2: User[] | undefined = new Array()
 
-  public transform(members: User[] | undefined, loggedUser: User): User[] | undefined {
-    members?.map(u => {
-    if (u.surname === loggedUser.surname && u.firstname === loggedUser.firstname) { this.user = u }
-      })
-    const index = members?.indexOf(this.user)
-    console.log(index)
-    //  members?[index]=members?[0]
-    // members?[0]=this.user
-   // if (index?.valueOf) { }
- //   members = members?.concat(this.user)
-
-    return members
+  public transform(members: User[], loggedUser: User): User[] {
+    const loggedUserInside = members.find(u => u.id === loggedUser.id)
+    const members2 = members.filter(u => u.id !== loggedUser.id)
+    if (loggedUserInside === undefined) { return members }
+    else {
+      members2.unshift(loggedUserInside)
+      return members2
+    }
   }
 
 }
