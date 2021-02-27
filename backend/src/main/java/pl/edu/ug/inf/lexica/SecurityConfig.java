47751e15d3a0cbbac62a939d827a5f12a08f22ca
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-02-27 17:38:10 +0100

    Basic authentication of requests

--- backend/src/main/java/pl/edu/ug/inf/lexica/SecurityConfig.java
+++ backend/src/main/java/pl/edu/ug/inf/lexica/SecurityConfig.java
@@ -1,11 +1,13 @@
 package pl.edu.ug.inf.lexica;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -23,18 +25,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
                 .jdbcAuthentication()
                 .dataSource(dataSource)
-                .usersByUsernameQuery("SELECT email, password, 1 FROM person WHERE username = ?;");
+                .usersByUsernameQuery("SELECT email as username, password, 1 FROM lexicauser WHERE email = ?;")
+                .authoritiesByUsernameQuery("SELECT email as username, 'USER' FROM lexicauser WHERE email = ?;");
     }
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
-                .httpBasic()
-                .and()
+                .cors().and()
+                .csrf().disable()
+                .httpBasic().and()
                 .authorizeRequests()
                 .antMatchers("/", "/index.html", "/user/login", "/register").permitAll()
-                .anyRequest().permitAll() //.authenticated()
-                .and().csrf().disable().cors().disable();
+                .anyRequest().authenticated();
+
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 }
 
--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 import { Observable, BehaviorSubject } from 'rxjs'
 import { User } from 'src/app/classes/user'
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { lexicaURL } from '../lexica.properties'
 import { Progress } from '../classes/progress'
 import { tap } from 'rxjs/operators'
@@ -10,9 +10,9 @@ import { tap } from 'rxjs/operators'
   providedIn: 'root',
 })
 export class UserService {
-  public  readonly emptyUser = new User('', '', '', '', '')
-  private readonly storage   = sessionStorage
-  private userSource         = new BehaviorSubject(this.loadSessionUser())
+  public readonly emptyUser = new User('', '', '', '', '')
+  private readonly storage = sessionStorage
+  private userSource = new BehaviorSubject(this.loadSessionUser())
 
   public constructor(private readonly http: HttpClient) { }
 
@@ -21,14 +21,20 @@ export class UserService {
     return sessionUser ? JSON.parse(sessionUser) : this.emptyUser
   }
 
+  public authHeader(): { headers: HttpHeaders } {
+    const user = this.loadSessionUser()
+    return {
+      headers: new HttpHeaders({
+        authorization: 'Basic ' + btoa(`${user.email}:${user.password}`)
+      }
+    )}
+  }
+
   private refreshUserSource(user: User = this.userSource.value): void {
     if (user !== this.emptyUser) {
-      this.http.get<User>(`${lexicaURL}/user/${user.id}`)
+      this.http.get<User>(`${lexicaURL}/user/${user.id}`, this.authHeader())
         .subscribe(
-          u => {
-            this.storage.setItem('user', JSON.stringify(u))
-            this.userSource.next(User.deserialize(u))
-          },
+          u   => { this.saveUserWithPassword(u, user.password) },
           err => {
             this.storage.removeItem('user')
             this.userSource.error(err) // Reset userSource after error
@@ -40,9 +46,15 @@ export class UserService {
   public login(email: string, password: string): Observable<User> {
     return this.http.post<User>(`${lexicaURL}/user/login`, { email, password })
       .pipe(tap(user => {
-        if (user) { this.refreshUserSource(user) }
+        if (user) { this.saveUserWithPassword(user, password) }
         else { throw new Error() }
       }))
+  }
+
+  private saveUserWithPassword(user: User, password: string): void {
+    const userWithPassword = { ...user, password }
+    this.storage.setItem('user', JSON.stringify(userWithPassword))
+    this.userSource.next(User.deserialize(userWithPassword as User))
   }
 
   public logout(): void {
@@ -51,13 +63,16 @@ export class UserService {
   }
 
   public setUser(user: User): void {
-    this.http.put<Progress>(`${lexicaURL}/user/${user.id}`, { ...user, password: '123987dasjk'})
+    this.http.put<Progress>(`${lexicaURL}/user/${user.id}`, user, this.authHeader())
       .subscribe(_ => this.refreshUserSource())
   }
 
   public addProgress(progress: Progress): Observable<Progress> {
     const userId = this.userSource.getValue().id
-    return this.http.put<Progress>(`${lexicaURL}/user/${userId}/progress`, progress)
+    return this.http.put<Progress>(
+      `${lexicaURL}/user/${userId}/progress`,
+      progress,
+      this.authHeader())
   }
 
   public get user(): Observable<User> {
