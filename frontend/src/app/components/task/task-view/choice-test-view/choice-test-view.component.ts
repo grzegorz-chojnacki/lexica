612da24122228f83d2e2273022e7bcc1e172e089
nwojtania <natalia-wojtania@wp.pl>
2021-03-14 23:19:13 +0100

    counting correct answers

--- frontend/src/app/components/task/task-view/choice-test-view/choice-test-view.component.ts
+++ frontend/src/app/components/task/task-view/choice-test-view/choice-test-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core'
 import { Task } from 'src/app/classes/task'
 import { ChoiceTestTask } from 'src/app/classes/task-type'
 import { ChoiceTest } from 'src/app/classes/example'
+import { newArray } from '@angular/compiler/src/util'
 
 @Component({
   selector: 'app-choice-test-view',
@@ -12,6 +13,7 @@ export class ChoiceTestViewComponent implements OnInit {
   @Input() public task!: Task<ChoiceTest>
 
   public counter = 0
+ public correctAnswer!: Array<string>
   public constructor() { }
 
   public ngOnInit(): void {
@@ -19,9 +21,17 @@ export class ChoiceTestViewComponent implements OnInit {
     this.task.examples.filter(example => example.decoys.push(example.answer))
     // random order of answers
     this.task.examples.filter(ex => ex.decoys.sort((a, b) => 0.5 - Math.random()))
+    this.correctAnswer = newArray(this.task.examples.length)
   }
 public sum(): void {
-
+  for (let i = 0; i < this.task.examples.length; i++) {
+  if (this.correctAnswer[i] === this.task.examples[i].answer)
+  {
+    console.log('dobrze' + this.correctAnswer[i] )
+    this.counter++
+  }
+}
+ console.log('razem:'+this.counter)
 }
 
 }
