de5e40dc99e9f28622b4f0079cb0db2cd503b431
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-03-16 17:26:40 +0100

    Removed unnecessary dependency + code formatting

--- frontend/src/app/components/task/task-view/choice-test-view/choice-test-view.component.ts
+++ frontend/src/app/components/task/task-view/choice-test-view/choice-test-view.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core'
 import { Task } from 'src/app/classes/task'
 import { ChoiceTest } from 'src/app/classes/example'
-import { newArray } from '@angular/compiler/src/util'
 import { TaskViewComponent } from '../task-view'
 import { MatDialog } from '@angular/material/dialog'
 import { TaskSummaryComponent } from '../../task-summary/task-summary.component'
@@ -24,23 +23,20 @@ export class ChoiceTestViewComponent extends TaskViewComponent implements OnInit
     this.task.examples.filter(example => example.decoys.push(example.answer))
     // random order of answers
     this.task.examples.filter(ex => ex.decoys.sort((a, b) => 0.5 - Math.random()))
-    this.correctAnswer = newArray(this.task.examples.length)
+    this.correctAnswer = []
   }
-public sum(): void {
-  for (let i = 0; i < this.task.examples.length; i++) {
-  if (this.correctAnswer[i] === this.task.examples[i].answer)
-  {
-    this.knewList.push(this.task.examples[i])
+
+  public sum(): void {
+    for (let i = 0; i < this.task.examples.length; i++) {
+      if (this.correctAnswer[i] === this.task.examples[i].answer) {
+        this.knewList.push(this.task.examples[i])
+      }
+    }
+
+    this.dialog.open(TaskSummaryComponent, {
+      disableClose: true,
+      data: { knewList: this.knewList, task: this.task },
+      width: '500px'
+    }).afterClosed().subscribe(progress => this.onSubmit.emit(progress))
   }
-}
- // console.log('razem:' + this.counter)
-  this.dialog.open(TaskSummaryComponent, {
-    disableClose: true,
-    data: {
-      knewList: this.knewList,
-      task: this.task
-    },
-    width: '500px'
-  }).afterClosed().subscribe(progress => this.onSubmit.emit(progress))
-}
 }
