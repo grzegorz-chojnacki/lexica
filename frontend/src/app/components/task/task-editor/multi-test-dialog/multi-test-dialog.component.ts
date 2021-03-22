7629bd2251064812139a77657af257d0b5a1e625
nwojtania <natalia-wojtania@wp.pl>
2021-03-22 23:47:45 +0100

    adding multi task

--- frontend/src/app/components/task/task-editor/multi-test-dialog/multi-test-dialog.component.ts
+++ frontend/src/app/components/task/task-editor/multi-test-dialog/multi-test-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core'
+import { FormBuilder, FormControl, Validators, FormArray } from '@angular/forms'
+import { MAT_DIALOG_DATA } from '@angular/material/dialog'
+import { MultiTest } from 'src/app/classes/example'
+
 
 @Component({
   selector: 'app-multi-test-dialog',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./multi-test-dialog.component.scss']
 })
 export class MultiTestDialogComponent implements OnInit {
+  public readonly multiTest = this.formBuilder.group({
+    question: new FormControl(this.card.question, [ Validators.required ]),
+    answers:  new FormArray(this.card.answers.map(a => new FormControl(a))),
+    decoys:  new FormArray(this.card.decoys.map(d => new FormControl(d)))
+  })
 
-  constructor() { }
+  get decoys(): FormArray {
+    return this.multiTest.get('decoys') as FormArray
+  }
+  get answers(): FormArray {
+    return this.multiTest.get('answers') as FormArray
+  }
+  public constructor(
+    private readonly formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) private readonly card: MultiTest) { }
+
 
   ngOnInit(): void {
   }
-
+  public createNewDecoy(): void {
+    this.decoys.push(this.formBuilder.control(''))
+  }
+  public deleteDecoy(decoyIndex: number): void {
+    this.decoys.removeAt(decoyIndex)
+  }
+  public createNewAnswer(): void {
+    this.answers.push(this.formBuilder.control(''))
+  }
+  public deleteAnswer(answerIndex: number): void {
+    this.answers.removeAt(answerIndex)
+  }
 }
