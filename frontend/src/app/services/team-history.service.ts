1c59f55ccb558b10f741feed9931765e38ef7532
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-03-28 21:21:15 +0200

    Added separate history entries for different users

--- frontend/src/app/services/team-history.service.ts
+++ frontend/src/app/services/team-history.service.ts
@@ -1,19 +1,31 @@
 import { Injectable } from '@angular/core'
 import { BehaviorSubject, Observable } from 'rxjs'
 import { Team } from '../classes/team'
+import { User } from '../classes/user'
+import { UserService } from './user.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamHistoryService {
+  private user?: User
   private readonly storage = localStorage
-  private readonly teamSource = new BehaviorSubject<Team[]>(this.loadTeams())
+  private readonly teamSource = new BehaviorSubject<Team[]>([])
 
-  public constructor() { }
+  private getTeamsKey(): string { return `${this.user?.id}-teams`}
 
-  public loadTeams(): Team[] {
-    const key = this.storage.getItem('teams')
-    return key ? JSON.parse(key) : []
+  public constructor(private readonly userService: UserService) {
+    this.userService.user.subscribe(user => {
+      this.user = user
+      this.loadTeams()
+    })
+  }
+
+  private loadTeams(): void {
+    if (this.user && this.user !== this.userService.emptyUser) {
+      const teams = this.storage.getItem(this.getTeamsKey())
+      this.teamSource.next(teams ? JSON.parse(teams) : [])
+    }
   }
 
   public get teams(): Observable<Team[]> {
@@ -48,6 +60,9 @@ export class TeamHistoryService {
   }
 
   private saveTeams(): void {
-    this.storage.setItem('teams', JSON.stringify(this.teamSource.value))
+    if (this.user && this.user !== this.userService.emptyUser) {
+      const teams = JSON.stringify(this.teamSource.value)
+      this.storage.setItem(this.getTeamsKey(), teams)
+    }
   }
 }
