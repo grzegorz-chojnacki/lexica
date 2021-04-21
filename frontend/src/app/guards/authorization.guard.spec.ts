1f81e2664e327ebf74163b7ff9ae4165fe8b06ba
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-04-21 21:02:37 +0200

    Added UnauthorizedGuard tests + Router spy fixes

--- frontend/src/app/guards/authorization.guard.spec.ts
+++ frontend/src/app/guards/authorization.guard.spec.ts
@@ -2,9 +2,11 @@ import { HttpClientModule } from '@angular/common/http'
 import { TestBed } from '@angular/core/testing'
 import { Router } from '@angular/router'
 import { RouterTestingModule } from '@angular/router/testing'
+import { StartViewComponent } from '../components/start-view/start-view.component'
+import { WorkspaceComponent } from '../components/workspace/workspace.component'
 import { UserService } from '../services/user.service'
 
-import { AuthorizedGuard } from './authorization.guard'
+import { AuthorizedGuard, UnauthorizedGuard } from './authorization.guard'
 
 const fakeUserService = () => ({
   logged: false,
@@ -12,18 +14,25 @@ const fakeUserService = () => ({
   logout() { this.logged = false }
 })
 
+const routerTestingModule = RouterTestingModule.withRoutes([
+  { path: '', component: StartViewComponent },
+  { path: 'workspace', component: WorkspaceComponent }
+])
+
 describe('AuthorizedGuard', () => {
   let guard: AuthorizedGuard
+  let router: Router
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         HttpClientModule,
-        RouterTestingModule
+        routerTestingModule
       ],
       providers: [{ provide: UserService, useValue: fakeUserService() }]
     })
     guard = TestBed.inject(AuthorizedGuard)
+    router = TestBed.inject(Router)
   })
 
   it('should be created', () => {
@@ -32,8 +41,6 @@ describe('AuthorizedGuard', () => {
 
   it('should activate only when user is logged', () => {
     const service = TestBed.inject(UserService)
-    expect(guard.canActivate()).toBeFalse()
-
     service.login('', '')
     expect(guard.canActivate()).toBeTrue()
 
@@ -43,8 +50,7 @@ describe('AuthorizedGuard', () => {
 
   it('should redirect when user is not authorized', () => {
     const service = TestBed.inject(UserService)
-    const router = TestBed.inject(Router)
-    spyOn(router, 'navigate')
+    spyOn(router, 'navigate').and.callThrough()
 
     service.logout()
     guard.canActivate()
@@ -54,10 +60,60 @@ describe('AuthorizedGuard', () => {
 
   it('should not redirect when user is authorized', () => {
     const service = TestBed.inject(UserService)
-    const router = TestBed.inject(Router)
-    spyOn(router, 'navigate')
+    spyOn(router, 'navigate').and.callThrough()
 
     service.login('', '')
+    guard.canActivate()
+
+    expect(router.navigate).not.toHaveBeenCalled()
+  })
+})
+
+describe('UnauthorizedGuard', () => {
+  let guard: UnauthorizedGuard
+  let router: Router
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        routerTestingModule
+      ],
+      providers: [{ provide: UserService, useValue: fakeUserService() }]
+    })
+    guard = TestBed.inject(UnauthorizedGuard)
+    router = TestBed.inject(Router)
+  })
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy()
+  })
+
+  it('should activate only when user is not logged', () => {
+    const service = TestBed.inject(UserService)
+
+    service.login('', '')
+    expect(guard.canActivate()).toBeFalse()
+
+    service.logout()
+    expect(guard.canActivate()).toBeTrue()
+  })
+
+  it('should redirect when user is authorized', () => {
+    const service = TestBed.inject(UserService)
+    spyOn(router, 'navigate').and.callThrough()
+
+    service.login('', '')
+    guard.canActivate()
+
+    expect(router.navigate).toHaveBeenCalledWith(['/workspace'])
+  })
+
+  it('should not redirect when user is unathorized', () => {
+    const service = TestBed.inject(UserService)
+    spyOn(router, 'navigate').and.callThrough()
+
+    service.logout()
     guard.canActivate()
 
     expect(router.navigate).not.toHaveBeenCalled()
