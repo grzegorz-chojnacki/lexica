2e05f3fe62f6d31d75f48de96333aebdf68557ac
nwojtania <natalia-wojtania@wp.pl>
2021-05-01 23:39:34 +0200

    registration and formatting

--- backend/src/test/java/pl/edu/ug/inf/lexica/api/UserControllerTest.java
+++ backend/src/test/java/pl/edu/ug/inf/lexica/api/UserControllerTest.java
@@ -2,49 +2,30 @@ package pl.edu.ug.inf.lexica.api;
 
 
 
-import org.junit.jupiter.api.Assertions;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.util.Assert;
-import pl.edu.ug.inf.lexica.domain.User;
 import pl.edu.ug.inf.lexica.service.TeamService;
 import pl.edu.ug.inf.lexica.service.UserService;
 
+import java.nio.charset.Charset;
 import java.security.Principal;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import static org.hamcrest.Matchers.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
@@ -63,22 +44,13 @@ class UserControllerTest {
     @MockBean
     private Principal principal;
 
+
 //    @Test
 //    void getTeams() {
 //    }
 
     @Test
     void getUser() throws Exception {
-//        UserService userService = Mockito.mock(UserService.class);
-      //  crypt = new BCryptPasswordEncoder();
-       User user = new User("Jan", "Kowalski", "jkowalski", "opBUs", "#D2326A");
-
-  //     user.setPassword(crypt.encode(user.getPassword()));
-//        when(userService.get("jkowalski")).thenReturn(Optional.of(user));
-//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-//        UserController userController = new UserController(userService,bCryptPasswordEncoder);
-//        Optional<User> optionalUser = userController.getUser(() -> "jkowalski");
-//        assertEquals(Optional.of(user),optionalUser);
         // Setup our mocked service
         doReturn(Optional.empty()).when(service).get(principal);
 
@@ -94,9 +66,26 @@ class UserControllerTest {
 //    void login() {
 //    }
 //
-//    @Test
-//    void register() {
-//    }
+public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+    @Test
+    void register() throws Exception {
+        Map<String, String> registration = new HashMap<>();
+        registration.put("username","xd");
+        registration.put("password","xd");
+        registration.put("surname","xd");
+        registration.put("firstname","xd");
+        registration.put("color","xd");
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow =  mapper.writer().withDefaultPrettyPrinter();
+        String requestJson= ow.writeValueAsString(registration);
+        mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(requestJson))
+                // Validate the response code
+                .andExpect(status().is(200));
+
+    }
 //
 //    @Test
 //    void addProgress() {
