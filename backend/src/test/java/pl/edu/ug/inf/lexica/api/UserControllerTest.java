5cf1900edefb20ba7359197ec650b98081859b3c
nwojtania <natalia-wojtania@wp.pl>
2021-05-07 19:51:46 +0200

    get user filled

--- backend/src/test/java/pl/edu/ug/inf/lexica/api/UserControllerTest.java
+++ backend/src/test/java/pl/edu/ug/inf/lexica/api/UserControllerTest.java
@@ -4,6 +4,8 @@ package pl.edu.ug.inf.lexica.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +15,8 @@ import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pl.edu.ug.inf.lexica.domain.User;
+import pl.edu.ug.inf.lexica.repository.UserRepository;
 import pl.edu.ug.inf.lexica.service.TeamService;
 import pl.edu.ug.inf.lexica.service.UserService;
 
@@ -23,6 +27,8 @@ import java.util.Map;
 import java.util.Optional;
 
 import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -43,14 +49,30 @@ class UserControllerTest {
     @MockBean
     private Principal principal;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    private User user1;
+
 
     //    @Test
 //    void getTeams() {
 //    }
     public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
 
+    @BeforeEach
+    void setup() {
+        user1 = new User("Jan", "Kowalski", "janK", "xyz123", "#D2326A");
+        userRepository.saveAndFlush(user1);
+    }
+
+    @AfterEach
+    void del() {
+        userRepository.deleteAll();
+    }
+
     @Test
-    void getUser() throws Exception {
+    void getEmptyUser() throws Exception {
         // Setup our mocked service
         doReturn(Optional.empty()).when(service).get(principal);
 
@@ -58,16 +80,37 @@ class UserControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/user")
                 .principal(principal))
                 // Validate the response code
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(print());
+
+    }
+
+    @Test
+    void getUser() throws Exception {
+        // Setup our mocked service
+        doReturn(Optional.of(user1)).when(service).get(principal);
+
+        // Execute the GET request
+        mockMvc.perform(MockMvcRequestBuilders.get("/user")
+                .principal(principal))
+                // Validate the response code
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstname").value("Jan"))
+                .andExpect(jsonPath("$.surname").value("Kowalski"))
+                .andExpect(jsonPath("$.username").value("janK"))
+                .andExpect(jsonPath("$.color").value("#D2326A"))
+                .andDo(print());
 
     }
 
 
     @Test
     void login() throws Exception {
+
+        //userRepository.findByUsername("janK").get();
         Map<String, String> user = new HashMap<>();
-        user.put("username", "janK");
-        user.put("password", "xyz123");
+        user.put("username", "janKc");
+        user.put("password", "xyz123c");
 
 
         ObjectMapper mapper = new ObjectMapper();
@@ -77,7 +120,8 @@ class UserControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.post("/user/login")
                 .contentType(APPLICATION_JSON_UTF8)
                 .content(requestJson))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 
     @Test
@@ -94,10 +138,12 @@ class UserControllerTest {
         String requestJson = ow.writeValueAsString(registration);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/user/register")
-                .contentType(APPLICATION_JSON_UTF8)
-                .content(requestJson))
+                .content(requestJson)
+                .contentType(APPLICATION_JSON_UTF8))
                 // Validate the response code
-                .andExpect(status().is(200));
+                .andExpect(status().is(200))
+                .andDo(print());
+
 
     }
 //
