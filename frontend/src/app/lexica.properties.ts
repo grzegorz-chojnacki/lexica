46c11845a6c6efbc525723a4f8be0bcd60b11d71
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-15 18:31:17 +0200

    Added deploy script

--- frontend/src/app/lexica.properties.ts
+++ frontend/src/app/lexica.properties.ts
@@ -1,2 +1,7 @@
-export const lexicaURL = 'http://localhost:8080/api'
+import { environment } from "src/environments/environment"
+
+export const lexicaURL = (environment.production)
+  ? 'http://153.19.8.161:80/api'
+  : 'http://localhost:8080/api'
+
 export const snackBarDuration = 2000
