8712b2b6bae31ea0810957e159bafcf7c7880694
nwojtania <natalia-wojtania@wp.pl>
2021-05-17 23:45:00 +0200

    context loads

--- backend/src/test/java/pl/edu/ug/inf/lexica/LexicaApplicationTests.java
+++ backend/src/test/java/pl/edu/ug/inf/lexica/LexicaApplicationTests.java
@@ -1,13 +1,24 @@
 package pl.edu.ug.inf.lexica;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import pl.edu.ug.inf.lexica.api.TeamController;
+import pl.edu.ug.inf.lexica.api.UserController;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class LexicaApplicationTests {
+	@Autowired
+	private UserController userController;
+	@Autowired
+	private TeamController teamController;
 
 	@Test
 	void contextLoads() {
+		assertThat(userController).isNotNull();
+		assertThat(teamController).isNotNull();
 	}
 
 }
