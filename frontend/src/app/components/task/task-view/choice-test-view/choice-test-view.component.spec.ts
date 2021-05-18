fd5d9ac6b144a861909f40bd5754697ef6ba024f
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-18 20:54:45 +0200

    Added ChoiceTestView tests

--- frontend/src/app/components/task/task-view/choice-test-view/choice-test-view.component.spec.ts
+++ frontend/src/app/components/task/task-view/choice-test-view/choice-test-view.component.spec.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { FormsModule } from '@angular/forms'
 import { MatCardModule } from '@angular/material/card'
-import { MatDialogModule } from '@angular/material/dialog'
+import { MatDialog, MatDialogModule } from '@angular/material/dialog'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatListModule } from '@angular/material/list'
 import { MatRadioModule } from '@angular/material/radio'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import { choiceTestTask } from 'src/app/test-data'
+import { TaskSummaryComponent } from '../../task-summary/task-summary.component'
 import { ChoiceTestViewComponent } from './choice-test-view.component'
-
 
 describe('ChoiceTestViewComponent', () => {
   let component: ChoiceTestViewComponent
@@ -15,11 +19,15 @@ describe('ChoiceTestViewComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         MatDialogModule,
+        CommonModule,
+        NoopAnimationsModule,
         MatCardModule,
+        MatDividerModule,
         MatRadioModule,
+        MatListModule,
         FormsModule
       ],
-      declarations: [ ChoiceTestViewComponent ]
+      declarations: [ ChoiceTestViewComponent, TaskSummaryComponent ]
     }).compileComponents()
   })
 
@@ -32,5 +40,16 @@ describe('ChoiceTestViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should accumulate knew examples and open dialog', () => {
+    const dialog = TestBed.inject(MatDialog)
+    spyOn(dialog, 'open').and.callThrough()
+
+    component.correctAnswer = [choiceTestTask.examples[0].answer]
+    component.sum()
+
+    expect(component.knewList.length).toBe(1)
+    expect(dialog.open).toHaveBeenCalled()
   })
 })
