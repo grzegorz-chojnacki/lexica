b657f1e1f410114be1223ef618778dd21cef230e
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-18 21:26:15 +0200

    Added MulitTestView tests

--- frontend/src/app/components/task/task-view/multi-test-view/multi-test-view.component.spec.ts
+++ frontend/src/app/components/task/task-view/multi-test-view/multi-test-view.component.spec.ts
@@ -2,8 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { FormsModule } from '@angular/forms'
 import { MatCardModule } from '@angular/material/card'
 import { MatCheckboxModule } from '@angular/material/checkbox'
-import { MatDialogModule } from '@angular/material/dialog'
+import { MatDialog, MatDialogModule } from '@angular/material/dialog'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatListModule } from '@angular/material/list'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import { multiTestTask } from 'src/app/test-data'
+import { TaskSummaryComponent } from '../../task-summary/task-summary.component'
 import { MultiTestViewComponent } from './multi-test-view.component'
 
 describe('MultiChoiceTestViewComponent', () => {
@@ -14,11 +18,14 @@ describe('MultiChoiceTestViewComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         MatDialogModule,
+        NoopAnimationsModule,
         MatCheckboxModule,
+        MatListModule,
+        MatDividerModule,
         FormsModule,
         MatCardModule,
       ],
-      declarations: [MultiTestViewComponent]
+      declarations: [MultiTestViewComponent, TaskSummaryComponent]
     }).compileComponents()
   })
 
@@ -31,5 +38,40 @@ describe('MultiChoiceTestViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should build multi test controls', () => {
+    component.ngOnInit()
+
+    expect(component.controls.length).toBe(component.task.examples.length)
+    expect(component.controls.every(c => component.task.examples.includes(c.example)))
+    expect(component.controls.every(c => c.checkboxes.every(ch => ch.value === false)))
+  })
+
+  it('should count knew examples and open dialog on sum', () => {
+    const dialog = TestBed.inject(MatDialog)
+    spyOn(dialog, 'open').and.callThrough()
+
+    const task = component.task
+    component.ngOnInit()
+
+    task.examples.forEach(example => {
+      const control = component.controls
+        .find(control => control.example === example)
+
+      if (control) {
+        example.answers.forEach(answer =>
+          control.checkboxes.forEach(chechbox => {
+            if (answer === chechbox.option) {
+              chechbox.value = true
+            }
+        }))
+      }
+    })
+
+    component.sum()
+
+    expect(component.knewList.length).toBe(component.task.examples.length)
+    expect(dialog.open).toHaveBeenCalled()
   })
 })
