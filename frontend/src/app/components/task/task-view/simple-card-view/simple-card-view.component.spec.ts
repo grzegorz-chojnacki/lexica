e19934579de4e73580c3c25b6119b6ded0d6795e
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-18 21:33:47 +0200

    Added SimpleCardView tests

--- frontend/src/app/components/task/task-view/simple-card-view/simple-card-view.component.spec.ts
+++ frontend/src/app/components/task/task-view/simple-card-view/simple-card-view.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { MatCardModule } from '@angular/material/card'
-import { MatDialogModule } from '@angular/material/dialog'
+import { MatDialog, MatDialogModule } from '@angular/material/dialog'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import { simpleCardTask } from 'src/app/test-data'
+import { TaskSummaryComponent } from '../../task-summary/task-summary.component'
 import { SimpleCardViewComponent } from './simple-card-view.component'
 
 describe('SimpleCardViewComponent', () => {
@@ -12,9 +14,10 @@ describe('SimpleCardViewComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         MatDialogModule,
+        NoopAnimationsModule,
         MatCardModule
       ],
-      declarations: [SimpleCardViewComponent],
+      declarations: [SimpleCardViewComponent, TaskSummaryComponent],
     }).compileComponents()
   })
 
@@ -27,5 +30,31 @@ describe('SimpleCardViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should set next card and not add previous to known', () => {
+    component.nextCard(false)
+
+    expect(component.counter).toBe(1)
+    expect(component.knewList.length).toBe(0)
+  })
+
+  it('should set next card and add previous to known', () => {
+    component.nextCard(true)
+
+    expect(component.counter).toBe(1)
+    expect(component.knewList.length).toBe(1)
+    expect(component.knewList).toContain(component.task.examples[0])
+  })
+
+  it('should open dialog on after last card', () => {
+    const dialog = TestBed.inject(MatDialog)
+    spyOn(dialog, 'open').and.callThrough()
+    const examples = component.task.examples
+
+    examples.forEach(example => component.nextCard(true))
+
+    expect(JSON.stringify(component.knewList)).toBe(JSON.stringify(examples))
+    expect(dialog.open).toHaveBeenCalledTimes(1)
   })
 })
