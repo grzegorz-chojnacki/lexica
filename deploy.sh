91b9de6a0a62d1fd6c0946c36fe9027355837886
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-05-31 09:01:06 +0000

    Fixed copy not overwriting

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ ROOT=$(dirname $(realpath -s "$0"))
 cd $ROOT/frontend \
 && npm install \
 && npm run build -- --prod \
+&& rm -R $ROOT/backend/src/main/resources/public \
 && cp -R $ROOT/frontend/dist/lexica $ROOT/backend/src/main/resources/public \
 && cd $ROOT/backend \
 && mvn clean install \
